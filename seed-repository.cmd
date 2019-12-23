3dc9599644a8e6fe037192273eb851f2144a37cb
Martin Egli <martin@eglinet.ch>
2019-12-23 09:38:52 +0100

    Fix path bug

--- seed-repository.cmd
+++ seed-repository.cmd
@@ -57,14 +57,16 @@ IF NOT EXIST "Tools\Resources" (
 	git submodule add -f https://github.com/anorisoft/Cake.Resources.git Tools/Resources
 	powershell write-host
 )
+Endlocal
 
 IF EXIST "Tools\Resources" (
 	IF EXIST "Tools\GlobalSettings" (
 		IF EXIST "Tools\SeedRepository" (
 			IF EXIST "Tools\SeedRepository\seed-repository.cake" (
 				COPY Tools\SeedRepository\seed-repository.cake
-rem			powershell .\Tools\Resources\build.ps1 -Script Seed.cake -Verbosity Diagnostic -Target Seed
-				powershell .\Tools\Resources\build.ps1 -Script seed-repository.cake -Target Seed
+				powershell write-host -fore Green "Run cake script!"
+			powershell .\Tools\Resources\build.ps1 -Script seed-repository.cake -Verbosity Diagnostic -Target Seed
+rem					powershell .\Tools\Resources\build.ps1 -Script seed-repository.cake -Target Seed
 			) ELSE (
 				powershell write-host -fore Red "File Tools\GlobalSettings\seed-repository.cake not exist."
 			)
@@ -78,5 +80,5 @@ rem			powershell .\Tools\Resources\build.ps1 -Script Seed.cake -Verbosity Diagno
 	powershell write-host -fore Red "Directory Tools\Resources not exist."
 )
 pause
-Endlocal
+
 
